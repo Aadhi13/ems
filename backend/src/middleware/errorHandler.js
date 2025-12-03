160e697c39571e09affbfc3dc9f8feebb6d505a2
Adhil Ameen <adhilameen@proton.me>
2025-12-03 21:43:58 +0530

    refactor: Add better logging in errorHandler
    Added simple message in console.log on the errorHandler middleware

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err);
+  console.error("Error caught in errorHandler: ", err);
   res.status(500).json({ error: "Internal Server Error" });
 };
 
